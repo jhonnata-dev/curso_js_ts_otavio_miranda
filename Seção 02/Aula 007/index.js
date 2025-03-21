2315165ada36a2080ca4850bbe1efa46155254ae
Jhonnata-dev <jhonnatadmartins@gmail.com>
2025-03-20 22:24:35 -0300

    Adicionanprimeiras aulas

---
+++ Seção 02/Aula 007/index.js
@@ -0,0 +1,23 @@
+// Console.log - A primeira coisa que você vai ver em JS
+console.log('Jhonnata');
+console.log("Jhonnata");
+// Para transformar um campo em uma string pode usar '' ou ""
+
+console.log(`Jhonnata`);
+// Também é possivel exibir um texto usando crases ``
+
+// Para exibir aspas dentro de uma string basta inverter
+console.log('Jhonnata "Dev"');
+console.log("Jhonnata 'Dev'");
+
+// Quando é usado as crases para transformar o campo em uma string
+// é possivel usar os 2 tipos de aspas
+console.log(`"Jhonnata" 'Dev'`);
+
+// Exibindo número
+console.log(123456); // number
+
+console.log(123.456); // number
+
+// exibindo mais de um valor
+console.log(12, 34.56, 'Jhonnata dev');
