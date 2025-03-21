9919c7c3bfbbd96e78c8730529d1fba7e8546f03
Jhonnata-dev <jhonnatadmartins@gmail.com>
2025-03-20 22:39:41 -0300

    AUla sobre comentárioss

---
+++ Seção 02/Aula 011/comentarios.js
@@ -0,0 +1,14 @@
+// Comentario
+Console.log('Olá mundo'); // Aqui tem outro comentário
+console.log('Linha 1');
+// Comentário
+
+//console.log('Linha 2');
+console.log('Linha 3');
+console.log('Linha 4');
+// Vai ser ignorado
+
+/* 
+Um bloco de código
+Linha de comentário
+*/
