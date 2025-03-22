0c84fcf690831c6075ae14b8f41e7c530cf56175
Jhonnata-dev <jhonnatadmartins@gmail.com>
2025-03-22 12:24:54 -0300

    Tipos de dados primitivos

---
+++ Seção 02/Aula 019/index.js
@@ -0,0 +1,14 @@
+// Tipos de dados primitivos
+
+const nome = 'Jhon'; // string
+const nome1 = "Jhon"; // string
+const nome2 = `Jhon`; // string
+
+const num1 = 10; // nunmber
+const num2 = 10.52; // number
+
+let nomeAluno; // undefined -> não aponta pra local nenhum na memoória
+
+const sobrenomeAluno = null; // Nulo -> não aponta pra local nenhum na memória
+
+const aprovado = false; // Boolean = true, false (lógico)
