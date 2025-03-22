2572dea2da9b92b08deff325f2f64b7ce1ebd4b9
Jhonnata-dev <jhonnatadmartins@gmail.com>
2025-03-22 14:48:43 -0300

    Exercicio

---
+++ Seção 02/Aula 022/js/script.js
@@ -0,0 +1,15 @@
+// minha resolução
+const num1 = Number(prompt('Digite um número:'));
+const num2 = Number(prompt('Digite outro número:'));
+
+const resultado = num1 + num2;
+
+alert(`O resultado da soma entre ${num1} e ${num2} é: ${resultado}`)
+
+// resolução do professor
+let num3 = (prompt('Digite um número:'));
+let num4 = (prompt('Digite outro número:'));
+num3 = Number(num3);
+num4 = Number(num4);
+
+alert(`O resultado da sua conta foi: ${num3 + num4}`)
