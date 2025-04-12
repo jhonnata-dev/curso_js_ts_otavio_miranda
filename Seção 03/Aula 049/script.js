985b4a41db32f182aedb9e9e8faf522e7c252bf3
Jhonnata-dev <jhonnatadmartins@gmail.com>
2025-04-12 07:00:36 -0300

    Diferenças entre

---
+++ Seção 03/Aula 049/script.js
@@ -0,0 +1,33 @@
+// var sobrenome;
+// const verdadeira = true;
+
+// // let tem escopo de bloco { ... bloco }
+// // vae tem escopo de função função
+
+// let nome = 'Jhonnata'  // criando
+// var nome2 = 'Jhonnata' // criando
+
+// if (verdadeira) {
+//     let nome = 'Otávio'; // criando
+//     var nome2 = 'Rogério' // redeclarando
+// }
+
+// if (verdadeira) {
+//     var nome2 = 'Ronaldo'; //redeclarando
+//     let nome  = 'Outra coisa';
+// }
+
+// console.log(nome, nome2);
+
+// function falaOi () {
+//     let nome = 'Luiz';
+//     var sobrenome = 'Miranda';
+// }
+
+// falaOi();
+
+sobrenome = 'Miranda'
+console.log(sobrenome);
+
+let sobrenome = 'Miranda';
+console.log(sobrenome);
