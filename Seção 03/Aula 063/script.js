487f6f9ffa487b5c132ec1e8cb50f721756b701f
Jhonnata-dev <jhonnatadmartins@gmail.com>
2025-04-16 23:48:17 -0300

    Tratando r laçando eeros

---
+++ Seção 03/Aula 063/script.js
@@ -0,0 +1,9 @@
+/* TRATANDO E LANÇANDO ERROS (TRY, CATCH, THROW) */
+
+// try -> Tenta executar o código
+try {
+    console.log(naoExisto);
+} catch(err) { // vai executar caso oconteça algum erro
+    console.log('naoExist não existe.');
+    
+}
